a058dfddf0ed846dae5026a6dc148223c1477f41
Yanina Walendzik <walendzikyanina@gmail.com>
2025-06-15 14:22:35 -0300

    Primer cambio 15-06

--- src/app/class/persona.ts
+++ src/app/class/persona.ts
@@ -2,5 +2,6 @@ export class Persona {
     public nombre!:string;
     public password!:string;
     public apellido!:string;
+    public usuario! : string;
  
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Persona } from '../class/persona';
+import { Router } from '@angular/router';
+import { text } from 'stream/consumers';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+ 
+  constructor(public router: Router) {  }
+   
+  public usuario : Persona = new Persona ();
 
+  public loguear() {
+    let usuarios: Array<Persona> = JSON.parse(localStorage.getItem('usuarios') ?? "[]");
+
+    if (!usuarios.some(u => u.usuario.toLowerCase()== this.usuario.usuario.toLowerCase()&& u.password == this.usuario.password
+  )){
+     alert ("Error");
+      return;
+    }
+    this.router.navigateByUrl("suamdor");
+  }
+  public mostrarPass(){
+
+    if (this.tipo == "password")
+      this.tipo = "text";
+    else 
+    this.tipo ="password";
+  
+  }
+  public mensaje: string = "Bienvenidos";
+  public password: string = "";
+  public tipo: string = "password";
+
+  public aceptar(): void {
+    alert(this.password);
+  }
+  public boton(): void {
+    this.tipo = "button";
+    
+  }
 
 }
